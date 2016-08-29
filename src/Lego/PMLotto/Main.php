fc6f81add597e3dca913e1cd61059bdb0739149a
Legoburner73 <legoplaysmcmail@gmail.com>
2016-08-29 11:39:33 +0100

    Create Main.php

---
+++ src/Lego/PMLotto/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Lego\PMLotto;
+
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener {
+
+}
