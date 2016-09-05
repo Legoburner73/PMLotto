ec7119a3ebf266f404bdcdd7326132a6a8ba2493
Legoburner73 <legoplaysmcmail@gmail.com>
2016-09-05 16:30:22 +0100

    Update Main.php

--- src/Lego/PMLotto/Main.php
+++ src/Lego/PMLotto/Main.php
@@ -7,9 +7,17 @@ use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
+use SAM\provider\XFDataProvider;
 
 class Main extends PluginBase implements Listener {
-
+  public $samapi;
+ public function onEnable()
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->samapi = $this->getServer()->getPluginManager()->getPlugin("SAMMY");
+    }
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+      
+    }
 }
